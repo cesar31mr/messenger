43b80d4669c60b9cb323249b53cd0365cbf30027
cesar <cesar31mr@gmail.com>
2023-07-21 11:57:20 -0600

    17. Login y validacion de ruta

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/usermodel";
+import { UserService } from "../../services/user.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  public user!: User;
+  public token!: any;
+  public identity!: any;
+  public data_user: any;
+
+  constructor(
+    private _userService : UserService,
+    private _router: Router
+  ){
+    this.data_user = this._userService.getIdentity();
+  }
+
+  ngOnInit(): void {
+    this.user = new User('1', '', '', '', '', '', '', '', '', false);
+
+    if(this.data_user){
+      this._router.navigate(['messenger']);
+    }
+  }
+
+  onSubmit(loginForm: any){
+    if(loginForm.value){
+      this._userService.login(this.user).subscribe(
+        response => {
+          console.log(response);
+          this.token = response.jwt;
+          this.identity = JSON.stringify(response.user);
+
+          localStorage.setItem("token", this.token);
+
+          this._userService.login(this.user, true).subscribe(
+            response => {
+              localStorage.setItem('identity', this.identity);
+              this._router.navigate(['messenger']);
+            }, error => {
+
+            }
+          )
+        }, error => {
+          console.log('error', error);
+        }
+      )
+    } else {
+      console.log('Datos no validos')
+    }
+  }
 }
--- front/src/app/services/user.service.ts
+++ front/src/app/services/user.service.ts
@@ -10,6 +10,8 @@ import { User } from "../models/usermodel";
 export class UserService {
 
   public url : string;
+  public token : any;
+  public identity : any;
 
   constructor(
     private _http : HttpClient
@@ -28,5 +30,38 @@ export class UserService {
 
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     return this._http.post(this.url + 'registro', obj, {headers: headers});
+   }
+
+   login(user: any, gettoken: any = null): Observable<any>{
+    const json = user;
+    if(gettoken != null){
+      user.gettoken = true;
+    }
+
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this._http.post(this.url + 'login', json, {headers: headers});
+   }
+
+   getToken(){
+    const token = localStorage.getItem('token');
+    if(token){
+      this.token = token;
+    } else {
+      this.token = null;
+    }
+
+    return this.token;
+   }
+
+   getIdentity(){
+    const identity = localStorage.getItem('identity');
+
+    if(identity){
+      this.identity = identity;
+    } else {
+      this.identity = null;
+    }
+
+    return this.identity;
    }
 }
